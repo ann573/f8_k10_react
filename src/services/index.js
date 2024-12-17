388cd4f11da39d0695f1aab4577b02f37c9f943d
ann573 <ngthanhan0507@gmail.com>
2024-12-17 18:22:48 +0700

    e

--- src/services/index.js
+++ src/services/index.js
@@ -2,7 +2,7 @@ import axios from "axios";
 
 export const instance = axios.create({
     
-    baseURL: "http://localhost:30000",
+    baseURL: "http://localhost:3000",
     // baseURL: "https://json-server-4ogm.onrender.com",
     headers: {
         "Content-Type": "application/json"
